312a07cb8cb1ee3a136bb7b8b8d93a0a0ae5f638
Ismael Martinez <ismael.efrain.martinez@gmail.com>
2021-03-03 00:18:41 -0500

    Basic TRS and ENS generated

--- CodeGen/googleApiCoordinates.js
+++ CodeGen/googleApiCoordinates.js
@@ -135,9 +135,7 @@ function exportMarkerForm(){
     let formHeader = document.createElement("h3");
     formHeader.innerText = "Device Information";
     deviceForm.appendChild(formHeader);
-
     let labelIndex = 1;
-    markedCandidates = {};
     for (const ll in coordinateList) {
         let id = labelIndex++;
         markerForm(id);
@@ -159,93 +157,164 @@ function readLocation(markerNumber){
 
 function readIoTTable(markerNumber){
     let tableResources = document.getElementById("dev" + markerNumber);
-    let resources = {};
+    let resourceDict = {};
     for (let i = 0; i < tableResources.rows.length; i++) {
         let objCells = tableResources.rows.item(i).cells;
         let attr = objCells.item(0).innerHTML;
         let attrVal = objCells.item(1).firstChild.value;
         // Check attr type
         if(attr.includes("File size")){
-            resources["fileSize"] = attrVal;
+            resourceDict["fileSize"] = attrVal;
         }else if(attr.includes("Local CPU")){
-            resources["localCPU"] = attrVal;
+            resourceDict["localCPU"] = attrVal;
         }else if(attr.includes("Local processing")){
-            resources["localProcessing"] = attrVal;
+            resourceDict["localProcessing"] = attrVal;
         }else if(attr.includes("Storage")){
-            resources["storageReq"] = attrVal;
+            resourceDict["storageReq"] = attrVal;
         }else if(attr.includes("RAM")){
-            resources["memReq"] = attrVal;
-        }else if(attr.includes("ComRad")){
-            resources["comRad"] = attrVal;
-        }else if(attr.includes("height")){
-            resources["height"] = attrVal;
+            resourceDict["memReq"] = attrVal;
+        }else if(attr.includes("Comm")){
+            resourceDict["comRad"] = attrVal;
+        }else if(attr.includes("Height")){
+            resourceDict["height"] = attrVal;
         }
     }
-    return resources;
+    return resourceDict;
+}
+
+function iotAttributes(){
+    return "attributes: {\n" +
+        "\t key: filseSize_mb, type: int \n" +
+        "\t key: local_CPU_ghz, type: float \n" +
+        "\t key: localProcessing_ms, type: int \n" +
+        "\t key: storageReq_mb, type: int \n" +
+        "\t key: ramReq_mb, type: int \n" +
+        "\t key: communicationRadius_m, type:float \n" +
+        "}";
+}
+
+function iotEntry(markerNumber, locationDict, resourceDict){
+    return `\tthing t${markerNumber} {\n` +
+        `\t\tlocation {\n` + // Location
+        `\t\t\tlatitude: ${locationDict["lat"]} \n` +
+        `\t\t\tlongitude: ${locationDict["lng"]} \n` +
+        `\t\t\theight: ${resourceDict["height"]} \n` +
+        `}\n\t\tattributes {\n` + // Resource attributes
+        `\t\t\tfileSize_mb: ${resourceDict["fileSize"]}\n` +
+        `\t\t\tlocal_CPU_ghz: ${resourceDict["localCPU"]}\n` +
+        `\t\t\tlocalProcessing_ms: ${resourceDict["localProcessing"]}\n` +
+        `\t\t\tstorageAvail_mb: ${resourceDict["storageAvail"]}\n` +
+        `\t\t\tramAvail_mb: ${resourceDict["memAvail"]}\n` +
+        `\t\t\tcommunicationRadius_m: ${resourceDict["comRad"]}\n` +
+        `\t\t\tserviceRate: ${resourceDict["serviceRate"]}\n` +
+        `\t\t}\n}`;
 }
 
 function readEdgeTable(markerNumber) {
     let tableResources = document.getElementById("dev" + markerNumber);
+    let resourceDict = {};
     for (let i = 0; i < tableResources.rows.length; i++) {
         let objCells = tableResources.rows.item(i).cells;
         let attr = objCells.item(0).innerHTML;
         let attrVal = objCells.item(1).firstChild.value;
         // Check attr type
         if(attr.includes("Local CPU")){
-            resources["localCPU"] = attrVal;
+            resourceDict["localCPU"] = attrVal;
         }else if(attr.includes("Local processing")){
-            resources["localProcessing"] = attrVal;
+            resourceDict["localProcessing"] = attrVal;
         }else if(attr.includes("Storage")){
-            resources["storageReq"] = attrVal;
+            resourceDict["storageAvail"] = attrVal;
         }else if(attr.includes("RAM")){
-            resources["memReq"] = attrVal;
-        }else if(attr.includes("ComRad")){
-            resources["comRad"] = attrVal;
-        }else if(attr.includes("serviceRate")){
-            resources["serviceRate"] = attrVal;
-        }else if(attr.includes("height")){
-            resources["height"] = attrVal;
+            resourceDict["memAvail"] = attrVal;
+        }else if(attr.includes("Comm")){
+            resourceDict["comRad"] = attrVal;
+        }else if(attr.includes("Service")){
+            resourceDict["serviceRate"] = attrVal;
+        }else if(attr.includes("Height")){
+            resourceDict["height"] = attrVal;
         }
     }
-    return resources;
+    return resourceDict;
+}
+
+function edgeAttributes(){
+    return "\tattributes: {\n" +
+        "\t\tkey: local_CPU_ghz, type: float \n" +
+        "\t\tkey: localProcessing_ms, type: int \n" +
+        "\t\tkey: storageAvail_mb, type: int \n" +
+        "\t\tkey: ramAvail_mb, type: int \n" +
+        "\t\tkey: communicationRadius_m, type:float \n" +
+        "\t\tkey: serviceRate, type: float \n" +
+        "}";
+}
+
+function edgeEntry(markerNumber, locationDict, resourceDict){
+    return `node n${markerNumber} {\n` +
+        `\tlocation {\n` + // Location
+        `\t\tlatitude: ${locationDict["lat"]} \n` +
+        `\t\tlongitude: ${locationDict["lng"]} \n` +
+        `\t\theight: ${resourceDict["height"]} \n` +
+        `}\n\tattributes {\n` + // Resource attributes
+        `\t\tlocal_CPU_ghz: ${resourceDict["localCPU"]}\n` +
+        `\t\tlocalProcessing_ms: ${resourceDict["localProcessing"]}\n` +
+        `\t\tstorageAvail_mb: ${resourceDict["storageAvail"]}\n` +
+        `\t\tramAvail_mb: ${resourceDict["memAvail"]}\n` +
+        `\t\tcommunicationRadius_m: ${resourceDict["comRad"]}\n` +
+        `\t\tserviceRate: ${resourceDict["serviceRate"]}\n` +
+        `\t}\n}`;
 }
 
 function exportMarkers(){
-    let labelIndex = 1;
-    markedCandidates = {};
+    let trs_candidates = {}; let trs_id = 0;
+    let ens_candidates = {}; let ens_id = 0;
     let coordinateJSON;
     for (const i in coordinateList){
         // Device i type
-        let location = readLocation(i);
+        let locationDict = readLocation(i);
         markerNumber = +i + +1;
         let dropdownTag = 'dropdown' + markerNumber;
         let dropdown = document.getElementById(dropdownTag);
         dropdownValue = dropdown.options[dropdown.selectedIndex].text;
         let isIoT = dropdownValue.localeCompare("IoT");
-        if(isIoT === 0){
-            readIoTTable(markerNumber);
-        } else{
-            readEdgeTable(markerNumber);
+        if(isIoT === 0){ // is IoT
+            let resourceDict = readIoTTable(markerNumber);
+            // Build TRS entry
+            trs_candidates[trs_id++] = iotEntry(markerNumber, locationDict, resourceDict);
+        } else{ // is Edge
+            let resourceDict = readEdgeTable(markerNumber);
+            // Build ENS entry
+            ens_candidates[ens_id++] = edgeEntry(markerNumber, locationDict, resourceDict);
         }
-
-        let lat = coordinateList[i]["lat"];
-        let lng = coordinateList[i]["lng"];
-        //var coordinateJSON = '{"latitude":' + lat.toString() + ', "longitude":' + lng.toString() + ', "label":' + labelIndex.toString() + '}';
-        let id = labelIndex++;
-        coordinateJSON = {
-                "latitude": lat.toString(),
-                "longitude": lng.toString(),
-                "height" : 1.0
-        };
-        markedCandidates[id] = coordinateJSON;
-        //markedCandidates.push(coordinateJSON);
     }
+
+    let trsFile;
+    if(trs_id > 0){
+        trsFile  = "Thing IoT \n\t " + iotAttributes();
+        for(let i_id = 0; i_id < trs_id; i_id++){
+            trsFile = trsFile + '\n\t' + trs_candidates[i_id];
+        }
+        trsFile = trsFile + '\n}';
+        console.log(trsFile);
+    }
+
+    let ensFile;
+    if(ens_id > 0){
+        ensFile = "Graph Edge \n\tnodeset {\n\t" + edgeAttributes();
+        for(let e_id = 0; e_id < ens_id; e_id++){
+            ensFile = ensFile + '\n\t' + ens_candidates[e_id];
+        }
+        ensFile = ensFile + '\n}\n}';
+        console.log(ensFile);
+    }
+
+
+
     //var jsonCandidatesString = markedCandidates.toString()
     //var jsonCandidates = '{"candidates":[' + jsonCandidatesString + ']}'
-    let jsonCandidates = {"location": markedCandidates};
-    let jsonCandidatesPretty = JSON.stringify(jsonCandidates, null, 2);
-    console.log(jsonCandidatesPretty);
-    let blob = new Blob([jsonCandidatesPretty],
-        {type: "text/plain;charset=utf-8"});
-    saveAs(blob, "location.json");
+    //let jsonCandidates = {"location": markedCandidates};
+    //let jsonCandidatesPretty = JSON.stringify(jsonCandidates, null, 2);
+    //console.log(jsonCandidatesPretty);
+    //let blob = new Blob([jsonCandidatesPretty],
+    //    {type: "text/plain;charset=utf-8"});
+   // saveAs(blob, "location.json");
 }
